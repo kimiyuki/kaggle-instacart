1346ca149d09e324e8c8ea74522a8c65b48eb9a3
kimiyuki shirai <kimiyuki@gmail.com>
2017-07-24 00:00:23 +0900

    create

--- post-prediction.R
+++ post-prediction.R
@@ -1,6 +1,8 @@
 source("config.R")
 gc()
 ret = read_csv(paste0('test-results/subtrain', TODAY, ".csv"))
+
+## how pred_ordered predict reordered?
 ret %>% select(reordered, pred_reordered) %>%
   mutate(pred_reordered = round(pred_reordered, 2)) %>% 
   group_by(pred_reordered) %>% summarise(u = mean(reordered), n = n()) %>% 
@@ -11,3 +13,7 @@ ret %>% select(reordered, pred_reordered) %>%
   mutate(pred_reordered = round(pred_reordered, 2)) %>% 
   group_by(pred_reordered) %>% summarise(u = mean(reordered), n = n()) %>% 
   ggplot(aes(pred_reordered, n)) + geom_bar(stat="identity") + scale_y_log10()
+
+
+###get recall
+ret %>% select(order_id)
