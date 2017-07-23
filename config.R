02b48c866878b93c3ca6ffba62909de47605de36
kimiyuki shirai <kimiyuki@gmail.com>
2017-07-23 12:58:15 +0900

    add LogLossBinary function

--- config.R
+++ config.R
@@ -2,3 +2,8 @@ library(tidyverse)
 library(feather)
 TODAY = Sys.Date() %>% gsub("-","",.)
 DATAPATH = "data/"
+
+LogLossBinary = function(actual, predicted, eps = 1e-15) {
+  predicted = pmin(pmax(predicted, eps), 1-eps)
+  - (sum(actual * log(predicted) + (1 - actual) * log(1 - predicted))) / length(actual)
+}
